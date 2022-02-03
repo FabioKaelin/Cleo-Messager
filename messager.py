679ccbab2e016fd07ccb695e50bff3758d012763
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-03 17:31:56 +0100

    Auf dem Selben geraet funktioniert es

--- messager.py
+++ messager.py
@@ -12,6 +12,13 @@ hostname = socket.gethostname()
 local_ip = socket.gethostbyname(hostname)
 altEmpfang = local_ip
 print("Eigene IP: " + local_ip)
+name = input("Name: ")
+
+sep = "#SEP#"
+end = "#END#"
+port = 9898
+buffer = 1024
+
 
 
 try:
@@ -25,6 +32,7 @@ try:
             altEmpfang = empfang
             var = input("Nachricht: ")
         elif empfang == "ende":
+            empfang = local_ip
             var = "ende"
         else:
             altEmpfang = empfang
@@ -32,16 +40,18 @@ try:
 
         if var != "":
             host = get_ip_address(empfang)
-            end = "#END#"
-            port = 9898
-            buffer = 1024
-
             if end in var:
-                print("Das isch ä Wornig: #end# isch im Dateiname ErRRORRRRRRRRRR")
+                print("Die Nachricht darf nicht #END# enthalten")
+                exit(-1)
+            if sep in var:
+                print("Die Nachricht darf nicht #sep# enthalten")
                 exit(-1)
 
             s = socket.socket()
             s.connect((host, port))
+            s.send(bytes(name, 'UTF-8'))
+
+            s.send(bytes(sep, 'UTF-8'))
 
             while True:
                 var_bytes = var[:buffer]
@@ -55,6 +65,6 @@ try:
             if var == "ende":
                 exit(-1)
         else:
-            print("Bitte eine Nachricht")
+            print("Eine Nachricht wird benötigt")
 except:
     print("Ende")
