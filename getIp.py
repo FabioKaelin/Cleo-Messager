dcf58ae2551b7b2471a1824dde46cdf86e52047a
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-04 10:04:56 +0100

    test

--- getIp.py
+++ getIp.py
@@ -17,3 +17,8 @@ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 local_ip = s.getsockname()[0]
 print(local_ip)
+
+from netifaces import interfaces, ifaddresses, AF_INET
+for ifaceName in interfaces():
+    addresses = [i['addr'] for i in ifaddresses(ifaceName).setdefault(AF_INET, [{'addr':'No IP addr'}] )]
+    print(' '.join(addresses))
