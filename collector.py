174e0edefc73c406677c106a066021aedc5cfd50
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-28 15:38:53 +0100

    Name Push added

--- collector.py
+++ collector.py
@@ -3,6 +3,8 @@ import os
 
 end = "#END#"
 sep = "#SEP#"
+nameTag = "#NAME#"
+messageTag = "#MES#"
 port = 9898
 buffer = 1024
 
@@ -21,33 +23,39 @@ print(local_ip)
 
 del s
 
-try:
-    while True:
-        s = socket.socket()
-        s.bind((local_ip, port))
-        s.listen()
+# try:
+while True:
+    sserver = socket.socket()
+    sserver.bind((local_ip, port))
+    sserver.listen()
 
-        client_socket, address = s.accept()
-        message = ""
-        while True:
-            text = client_socket.recv(buffer).decode()
-            message += text
-            if  end in text:
-                break
-        message = message.replace(end, "")
+    client_socket, address = sserver.accept()
+    message = ""
+    while True:
+        text = client_socket.recv(buffer).decode()
+        message += text
+        if  end in text:
+            break
+    message = message.replace(end, "")
+    if (messageTag in message):
+        message = message.replace(messageTag, "")
         messagesplit = message.split(sep)
 
         client_socket.close()
-        s.close
+        sserver.close
 
         if address[0] == local_ip and messagesplit[1] == "ende":
             break
 
         print(messagesplit[0] + ": " + messagesplit[1])
-        # print(address[0])
-        del s
-        del client_socket
-        del address
-except:
-    print("")
-    print("Ende")
+    elif (nameTag in message):
+        message = message.replace(sep, "")
+        message = message.replace(nameTag, "")
+        print(address[0] + " ist " + message)
+
+    del sserver
+    del client_socket
+    del address
+# except:
+#     print("")
+#     print("Ende")
--- messager.py
+++ messager.py
@@ -18,58 +18,76 @@ print("")
 
 sep = "#SEP#"
 end = "#END#"
+nameTag = "#NAME#"
+messageTag = "#MES#"
 port = 9898
 buffer = 1024
 notende = True
 
+for x in range(2, 255):
+    try:
+        ip = local_ip
+        ip = ip.split(".")
+        empfang1 = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(x)
+        print(empfang1)
+        host = get_ip_address(empfang1)
+        s = socket.socket()
+        s.settimeout(0.1)
+        s.connect((empfang1, port))
+        # print(host)
+        s.send(bytes(nameTag + sep + name + end, 'UTF-8'))
+        s.close()
+    except:
+        x = "a"
 
 
-try:
-    while notende:
 
-        empfang = input("Empfänger: ")
-        if empfang == "":
-            empfang = altEmpfang
-            # print(empfang)
-            var = input("Nachricht: ")
-        elif empfang == "ende":
-            empfang = local_ip
-            var = "ende"
-            notende = False
-        else:
-            altEmpfang = empfang
-            var = input("Nachricht: ")
+# try:
+while notende:
 
-        if var != "":
-            host = get_ip_address(empfang)
-            # print(host)
-            host = empfang
-            # print(host)
-            if end in var:
-                print("Die Nachricht darf nicht #END# enthalten")
-                exit(-1)
-            if sep in var:
-                #172.20.107.85
-                print("Die Nachricht darf nicht #sep# enthalten")
-                exit(-1)
+    empfang = input("Empfänger: ")
+    if empfang == "":
+        empfang = altEmpfang
+        # print(empfang)
+        var = input("Nachricht: ")
+    elif empfang == "ende":
+        empfang = local_ip
+        var = "ende"
+        notende = False
+    else:
+        altEmpfang = empfang
+        var = input("Nachricht: ")
 
-            s = socket.socket()
-            s.connect((host, port))
-            # print(host)
-            s.send(bytes(name, 'UTF-8'))
+    if var != "":
+        host = get_ip_address(empfang)
+        # print(host)
+        host = empfang
+        # print(host)
+        if end in var:
+            print("Die Nachricht darf nicht #END# enthalten")
+            exit(-1)
+        if sep in var:
+            #172.20.107.85
+            print("Die Nachricht darf nicht #sep# enthalten")
+            exit(-1)
 
-            s.send(bytes(sep, 'UTF-8'))
+        s = socket.socket()
+        s.connect((host, port))
+        s.send(bytes(messageTag, 'UTF-8'))
+        s.send(bytes(name, 'UTF-8'))
 
-            while True:
-                var_bytes = var[:buffer]
-                var = var[buffer:]
-                if var_bytes == "":
-                    s.sendall(bytes(end, 'UTF-8'))
-                    break
-                s.sendall(bytes(var_bytes, 'UTF-8'))
-            s.close()
-            print("")
-        else:
-            print("Eine Nachricht wird benötigt")
-except:
-    print("Ende")
+        s.send(bytes(sep, 'UTF-8'))
+
+        while True:
+            var_bytes = var[:buffer]
+            var = var[buffer:]
+            if var_bytes == "":
+                s.sendall(bytes(end, 'UTF-8'))
+                break
+            s.sendall(bytes(var_bytes, 'UTF-8'))
+        s.close()
+        print("")
+    else:
+        print("Eine Nachricht wird benötigt")
+# except:
+#     print("Ende")
