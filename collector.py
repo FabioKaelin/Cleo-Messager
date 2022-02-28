ce3a45fbf6bb874e2f0189658561ac4b14aec718
FabioKaelin <90179796+FabioKaelin@users.noreply.github.com>
2022-02-28 17:54:50 +0100

    modify

--- collector.py
+++ collector.py
@@ -1,63 +1,82 @@
 import socket
 import os
+import threading
+import time
 
-end = "#END#"
-sep = "#SEP#"
-exitTag = "#EXIT#"
-nameTag = "#NAME#"
-messageTag = "#MES#"
-port = 9898
-buffer = 1024
 
-# hostname = socket.gethostname()
-# local_ip = socket.gethostbyname(hostname)
+def server():
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# s.connect(("8.8.8.8", 80))
-# local_ip = s.getsockname()[0]
-# print(local_ip)
+    end = "#END#"
+    sep = "#SEP#"
+    exitTag = "#EXIT#"
+    nameTag = "#NAME#"
+    messageTag = "#MES#"
+    logoutTag = "#LOGOUT#"
+    port = 9898
+    buffer = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.connect(("8.8.8.8", 80))
-local_ip = s.getsockname()[0]
-print(local_ip)
 
-del s
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+    print("Eigene IP: " + local_ip)
 
-# try:
-while True:
-    sserver = socket.socket()
-    sserver.bind((local_ip, port))
-    sserver.listen()
+    del s
 
-    client_socket, address = sserver.accept()
-    message = ""
+    # try:
     while True:
-        text = client_socket.recv(buffer).decode()
-        message += text
-        if  end in text:
-            break
-    message = message.replace(end, "")
-    if address[0] == local_ip and exitTag in message:
-            break
-    if (messageTag in message):
-        message = message.replace(messageTag, "")
-        messagesplit = message.split(sep)
+        sserver = socket.socket()
+        sserver.bind((local_ip, port))
+        sserver.listen()
+        client_socket, address = sserver.accept()
 
-        client_socket.close()
-        sserver.close
+        message = ""
+        while True:
+            text = client_socket.recv(buffer).decode()
+            message += text
+            if  end in text:
+                break
+        message = message.replace(end, "")
+        # print(message)
+        if (exitTag in message and address[0] == local_ip):
+            if (address[0] == local_ip):
+
+                message = message.replace(exitTag, "")
+                message = message.replace(logoutTag, "")
+                message = message.replace(sep, "")
+                message = message.replace(nameTag, "")
+                message = message.replace(messageTag, "")
+                print(message + " ist ausgeloggt ")
+                exit()
+        if (logoutTag in message):
+            message = message.replace(exitTag, "")
+            message = message.replace(logoutTag, "")
+            message = message.replace(sep, "")
+            message = message.replace(messageTag, "")
+            message = message.replace(nameTag, "")
+            print(message + " ist ausgeloggt")
+        if (messageTag in message):
+            message = message.replace(messageTag, "")
+            messagesplit = message.split(sep)
+
+            client_socket.close()
+            sserver.close
 
 
 
-        print(messagesplit[0] + ": " + messagesplit[1])
-    elif (nameTag in message):
-        message = message.replace(sep, "")
-        message = message.replace(nameTag, "")
-        print(address[0] + " ist " + message)
+            print(messagesplit[0] + ": " + messagesplit[1])
+        elif (nameTag in message):
+            message = message.replace(sep, "")
+            message = message.replace(nameTag, "")
+            print(address[0] + " ist " + message)
 
-    del sserver
-    del client_socket
-    del address
-# except:
-#     print("")
-#     print("Ende")
+        del sserver
+        del client_socket
+        del address
+    # except:
+    #     print("")
+    #     print("Ende")
+
+
+x = threading.Thread(target=server)
+x.start()
--- messager.py
+++ messager.py
@@ -1,12 +1,70 @@
 import os
 import socket
 import sys
+import threading
+import time
+
+NameToIP = []
 
 def get_ip_address(empfang):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((empfang, 80))
-    # print(s)
     return s.getsockname()[0]
+
+
+
+
+
+def NameListener():
+
+    end = "#END#"
+    sep = "#SEP#"
+    exitTag = "#EXIT#"
+    nameTag = "#NAME#"
+    messageTag = "#MES#"
+    buffer = 1024
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    local_ip = s.getsockname()[0]
+
+    del s
+
+    # try:
+    while True:
+        sserver = socket.socket()
+        sserver.bind((local_ip, 9899))
+        sserver.listen()
+        client_socket, address = sserver.accept()
+
+        message = ""
+        while True:
+            text = client_socket.recv(buffer).decode()
+            message += text
+            if end in text:
+                break
+        message = message.replace(end, "")
+        if (nameTag in message):
+            message = message.replace(sep, "")
+            message = message.replace(nameTag, "")
+            NameToIP.append([address[0], message])
+        if (logoutTag in message):
+            message = message.replace(sep, "")
+            message = message.replace(logoutTag, "")
+            NameToIP.pop([address[0], message])
+        if (exitTag in message):
+            if (address[0] == local_ip):
+                exit()
+
+        del sserver
+        del client_socket
+        del address
+    # except:
+    #     print("")
+    #     print("Ende")
+
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -24,26 +82,63 @@ end = "#END#"
 exitTag = "#EXIT#"
 nameTag = "#NAME#"
 messageTag = "#MES#"
+logoutTag = "#LOGOUT#"
 port = 9898
 buffer = 1024
 notende = True
 
-for x in range(2, 255):
-    try:
-        ip = local_ip
-        ip = ip.split(".")
-        empfang1 = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(x)
-        # print(empfang1)
-        host = get_ip_address(empfang1)
-        s = socket.socket()
-        s.settimeout(0.001)
-        s.connect((empfang1, port))
-        # print(host)
-        s.send(bytes(nameTag + sep + name + end, 'UTF-8'))
-        s.close()
-    except:
-        x = "a"
+def logoutIP9899():
+    for x in range(2, 255):
+        try:
+            ip = local_ip
+            ip = ip.split(".")
+            empfang1 = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(x)
+            host = get_ip_address(empfang1)
+            s = socket.socket()
+            s.settimeout(0.001)
+            s.connect((empfang1, 9899))
+            s.send(bytes(logoutTag + sep + name + end, 'UTF-8'))
+            s.close()
+        except:
+            x = "a"
 
+def sayIP9898():
+    for x in range(2, 255):
+        try:
+            ip = local_ip
+            ip = ip.split(".")
+            empfang1 = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(x)
+            host = get_ip_address(empfang1)
+            s = socket.socket()
+            s.settimeout(0.001)
+            s.connect((empfang1, port))
+            s.send(bytes(nameTag + sep + name + end, 'UTF-8'))
+            s.close()
+        except:
+            x = "a"
+
+def sayIP9899():
+    for x in range(2, 255):
+        try:
+            ip = local_ip
+            ip = ip.split(".")
+            empfang1 = str(ip[0]) + "." + str(ip[1]) + "." + str(ip[2]) + "." + str(x)
+            host = get_ip_address(empfang1)
+            s = socket.socket()
+            s.settimeout(0.001)
+            s.connect((empfang1, 9899))
+            s.send(bytes(nameTag + sep + name + end, 'UTF-8'))
+            s.close()
+        except:
+            x = "a"
+
+
+x = threading.Thread(target=sayIP9898)
+x.start()
+z = threading.Thread(target=sayIP9899)
+z.start()
+y = threading.Thread(target=NameListener)
+y.start()
 
 
 # try:
@@ -52,26 +147,59 @@ while notende:
     empfang = input("Empfänger: ")
     if empfang == "":
         empfang = altEmpfang
-        # print(empfang)
         var = input("Nachricht: ")
     elif empfang == "ende":
+        logoutIP9899()
         empfang = local_ip
         var = exitTag
+        host = get_ip_address(empfang)
+        host = empfang
+        if end in var:
+            print("Die Nachricht darf nicht #END# enthalten")
+            exit(-1)
+        if sep in var:
+            print("Die Nachricht darf nicht #sep# enthalten")
+            exit(-1)
+
+        s = socket.socket()
+        s.connect((host, 9899))
+        s.send(bytes(logoutTag, 'UTF-8'))
+        s.send(bytes(messageTag, 'UTF-8'))
+        s.send(bytes(name, 'UTF-8'))
+
+        s.send(bytes(sep, 'UTF-8'))
+
+        while True:
+            var_bytes = var[:buffer]
+            var = var[buffer:]
+            if var_bytes == "":
+                s.sendall(bytes(end, 'UTF-8'))
+                break
+            s.sendall(bytes(var_bytes, 'UTF-8'))
+        s.close()
+        print("")
+
+        var = exitTag
+
+
+
+
         notende = False
+        # break
     else:
+        for x in NameToIP:
+            if (x[1] == empfang):
+                empfang = x[0]
         altEmpfang = empfang
         var = input("Nachricht: ")
 
     if var != "":
         host = get_ip_address(empfang)
-        # print(host)
         host = empfang
-        # print(host)
         if end in var:
             print("Die Nachricht darf nicht #END# enthalten")
             exit(-1)
         if sep in var:
-            #172.20.107.85
             print("Die Nachricht darf nicht #sep# enthalten")
             exit(-1)
 
